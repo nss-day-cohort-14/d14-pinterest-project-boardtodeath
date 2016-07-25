9ab049d47112a6c52fc65b8ba1d2dca59ab67b36
Brian Doane <brianmdoane@gmail.com>
2016-07-25 14:09:20 -0500

    Some login control logic and authfactory creation with methods

--- app/controllers/LoginCtrl.js
+++ app/controllers/LoginCtrl.js
@@ -3,7 +3,13 @@
 app.controller('LoginCtrl', function($scope) {
 
 	$scope.login = function() {
-		
+		console.log("cluck");
+		AuthFactory.authWithProvider()
+			// Return of a promise
+	    .then(function(result) {
+	      $location.path("/boardView");
+	      $scope.$apply();
+			});
 	};
 
 });
